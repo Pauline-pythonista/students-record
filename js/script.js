dceed6037361afc68b5d8a800b9a76321061a406
Pauline BILOA <paulina@biloapauline01@gmail.com>
2022-09-08 02:59:26 +0100

    mise en place des fonctions pour les boutons d'appel en action : add, view, edit, delete

--- js/script.js
+++ js/script.js
@@ -1,18 +1,61 @@
-// Gestion des notes de l'étudiant
-// Ajouter et supprimer une matiere et sa note.
+/*
+Partie concentrée à la gestion des button call to actions pour :
+
+- modifier les données d'un etudiant
+- voir les info d'un etudiant specifique
+- supprimer un etudiant
+
+*/
+
+const btnNouveauEtudiant = document.getElementById("new-recordStudent");
+const zoneEnregistrementEtudiant = document.getElementById("zone-enregistrement-etudiant");
+const zoneAffichage = document.getElementById("zone-affichage");
+const zoneDetails= document.getElementById("zone-details");
+
+btnNouveauEtudiant.addEventListener('click', () =>{
+    zoneAffichage.setAttribute('style', 'display:none');
+    zoneEnregistrementEtudiant.setAttribute('style', 'display: block;');
+});
+
+//function qui permet d'ajouter / creer un etudiant
+function addStudent(){
+    zoneAffichage.setAttribute('style', 'display:none');
+    zoneEnregistrementEtudiant.setAttribute('style', 'display: block;');
+}
+
+//function qui permet de modifier les données d'un etudiant déjà enregistrer
+function editStudentInfo(){
+    zoneAffichage.setAttribute('style', 'display:none');
+    zoneEnregistrementEtudiant.setAttribute('style', 'display: block;');
+    document.getElementById("legend").innerText = "Modifier les données de l'étudiant";
+}
+
+//function qui permet de modifier les données d'un etudiant déjà enregistrer
+function viewStudentDetail(){
+    zoneAffichage.setAttribute('style', 'display:none');
+    zoneDetails.setAttribute('style', 'display: block;');
+}
+
+
+/* 
+Parie concentrée à la Gestion des notes de l'étudiant
+Ajouter et supprimer une matiere et sa note
+
+ */
+
 const addMatiere = document.getElementById("matiere");
 const addNote = document.getElementById("note");
 const addButton = document.getElementById("boutonAjouter");
 
 let matiereNoteData = {};
 
-// let selectedRow = null;
+let selectedRow = null; //au depart on a aucune ligne dans le tableau
 
 function getMatiereNoteFormData(){
     matiereNoteData["matiere"] = addMatiere.options[addMatiere.selectedIndex].text;
     matiereNoteData["note"] = addNote.value;
     // il faudra rajouter un control supplementaire de valeur
-    
+    return matiereNoteData;    
 }
 
 //cette dfonction doit etre amelioree
@@ -37,7 +80,7 @@ addButton.addEventListener('click',(event)=>{
     cell3 = newRow.insertCell(2);   
     cell3.innerHTML = `<ul class="list-inline m-0">
                         <li class="list-inline-item">
-                            <button class="btn btn-danger btn-sm rounded-0" type="button" data-bs-toggle="tooltip" data-bs-placement="top" title="Delete" id="btnDelete1"><i class="fa-solid fa-minus"></i></button>
+                            <button onclick="event.preventDefault();" class="btn btn-danger btn-sm rounded-0" type="button" data-bs-toggle="tooltip" data-bs-placement="top" title="Delete" id="btnDelete1"><i class="fa-solid fa-minus"></i></button>
                         </li>
                     </ul>`;
     cell3.classList.add("text-center"); 
@@ -79,15 +122,16 @@ function getStudentsInformationsFormData(){
     studentData["parcours"] = parcourStudent.options[parcourStudent.selectedIndex].text;
     studentData["taille"] = tailleStudent.value;
     studentData["poids"] = poidStudent.value;
-    studentData["matieresNotes"] = getMatiereNoteFormData();
+    studentData["matieresNotes"] = matiereNoteData;
 
+    return studentData;
 }
 
 //Enregistrer les informations d'un nouvel etudiant cree dans le tableau de la liste des etudiants
 saveButton.addEventListener('click', (event) =>{
     event.preventDefault();
     event.stopPropagation();
-    getStudentsInformationsFormData()
+    getStudentsInformationsFormData();
     let tableStudents = document.getElementById("table-students").getElementsByTagName("tbody")[0];
     let newRow = tableStudents.insertRow(tableStudents.length);
     cell1 = newRow.insertCell(0);
@@ -114,25 +158,33 @@ saveButton.addEventListener('click', (event) =>{
     cell8.classList.add("text-center");
     cell8.innerHTML = `<ul class="list-inline m-0">
                             <li class="list-inline-item">
-                                <button class="btn btn-primary btn-sm rounded-0" id="fa-plus" type="button" data-bs-toggle="tooltip" data-bs-placement="top" title="Add"><i class="fa-solid fa-plus"></i></button>
+                                <button onclick="event.preventDefault(); addStudent(); " class="btn btn-primary btn-sm rounded-0" id="fa-plus" type="button" data-bs-toggle="tooltip" data-bs-placement="top" title="Add"><i class="fa-solid fa-plus"></i></button>
                             </li>
                             <li class="list-inline-item">
-                                <button class="btn btn-success btn-sm rounded-0" id="fa-edit" type="button" data-bs-toggle="tooltip" data-bs-placement="top" title="Edit"><i class="fa fa-edit"></i></button>
+                                <button onclick="event.preventDefault(); editStudentInfo(); " class="btn btn-success btn-sm rounded-0" id="fa-edit" type="button" data-bs-toggle="tooltip" data-bs-placement="top" title="Edit"><i class="fa fa-edit"></i></button>
                             </li>
                             <li class="list-inline-item">
-                                <button class="btn btn-info btn-sm rounded-0" id="fa-view" type="button" data-bs-toggle="tooltip" data-bs-placement="top" title="View"><i class="fa-solid fa-eye"></i></button>
+                                <button onclick="event.preventDefault(); viewStudentDetail(); " class="btn btn-info btn-sm rounded-0" id="fa-view" type="button" data-bs-toggle="tooltip" data-bs-placement="top" title="View"><i class="fa-solid fa-eye"></i></button>
                             </li>
                             <li class="list-inline-item">
-                                <button class="btn btn-danger btn-sm rounded-0" type="button" data-bs-toggle="tooltip" data-bs-placement="top" title="Delete"><i class="fa-solid fa-minus"></i></button>
+                                <button onclick="event.preventDefault();" class="btn btn-danger btn-sm rounded-0" type="button" data-bs-toggle="tooltip" data-bs-placement="top" title="Delete"><i class="fa-solid fa-minus"></i></button>
                             </li>
                         </ul>`;
+    zoneAffichage.setAttribute('style', 'display:block');
+    zoneEnregistrementEtudiant.setAttribute('style', 'display: none');
 });
+
 
 
 //FUnction pour sauvegarder les données de l'étudiant dans le local storage
 function saveStudentsInfo(data){
     //code
    
+}
+
+//function pour recuperer les informations de l'étudiant depuis le local storage
+function getStudentsInfo(id){
+    //code
 }
 
 
